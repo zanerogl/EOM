388b535bacf9af3ba109477bad5afaf476a5389c
Zanerogl <102305838+zanerogl@users.noreply.github.com>
2022-06-11 14:29:11 +0800

    Add files via upload

---
+++ debug/moc_ip_camera_setting.cpp
@@ -0,0 +1,151 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'ip_camera_setting.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.14.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../5/ip_camera_setting.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'ip_camera_setting.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.14.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_ip_camera_setting_t {
+    QByteArrayData data[8];
+    char stringdata0[100];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_ip_camera_setting_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_ip_camera_setting_t qt_meta_stringdata_ip_camera_setting = {
+    {
+QT_MOC_LITERAL(0, 0, 17), // "ip_camera_setting"
+QT_MOC_LITERAL(1, 18, 4), // "send"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 11), // "std::string"
+QT_MOC_LITERAL(4, 36, 7), // "address"
+QT_MOC_LITERAL(5, 44, 4), // "port"
+QT_MOC_LITERAL(6, 49, 25), // "on_connect_button_clicked"
+QT_MOC_LITERAL(7, 75, 24) // "on_cancel_button_clicked"
+
+    },
+    "ip_camera_setting\0send\0\0std::string\0"
+    "address\0port\0on_connect_button_clicked\0"
+    "on_cancel_button_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ip_camera_setting[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       3,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    2,   29,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       6,    0,   34,    2, 0x08 /* Private */,
+       7,    0,   35,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, 0x80000000 | 3, 0x80000000 | 3,    4,    5,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void ip_camera_setting::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<ip_camera_setting *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->send((*reinterpret_cast< std::string(*)>(_a[1])),(*reinterpret_cast< std::string(*)>(_a[2]))); break;
+        case 1: _t->on_connect_button_clicked(); break;
+        case 2: _t->on_cancel_button_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (ip_camera_setting::*)(std::string , std::string );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&ip_camera_setting::send)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject ip_camera_setting::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_ip_camera_setting.data,
+    qt_meta_data_ip_camera_setting,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *ip_camera_setting::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ip_camera_setting::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ip_camera_setting.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int ip_camera_setting::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 3;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void ip_camera_setting::send(std::string _t1, std::string _t2)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))), const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t2))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
